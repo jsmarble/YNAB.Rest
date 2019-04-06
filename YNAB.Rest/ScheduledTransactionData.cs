42e1f6e9ba8b09e5290cd93f526a726b4ac69cc2
Jacob Winn <jmwinn21@gmail.com>
2019-04-06 16:03:10 -0400

    Update readme Add Scheduled Transacitions

---
+++ YNAB.Rest/ScheduledTransactionData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace YNAB.Rest
+{
+    public class ScheduledTransactionData
+    {
+        public ScheduledTransaction ScheduledTransaction { get; set; }
+    }
+}
---
+++ YNAB.Rest/ScheduledTransactionsData.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+namespace YNAB.Rest {
+    public class ScheduledTransactionsData {
+        public IList<ScheduledTransaction> ScheduledTransactions { get; set; }
+    }
+}
