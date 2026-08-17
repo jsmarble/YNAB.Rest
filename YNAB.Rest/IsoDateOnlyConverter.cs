4b4e2bcd4328d7480d7861c7bb98ca021097ee2f
Sean Lamberson <sean.lamberson@aumentumtech.com>
2026-08-17 12:51:04 -0400

    Fix issue with sending ISO date format to REST method

---
+++ YNAB.Rest/IsoDateOnlyConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace YNAB.Rest
+{
+    /// <inheritdoc />
+    public class IsoDateOnlyConverter : JsonConverter<DateTime>
+    {
+        private const string Format = "yyyy-MM-dd";
+
+        /// <inheritdoc />
+        public override void WriteJson(JsonWriter writer, DateTime value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString(Format));
+        }
+
+        /// <inheritdoc />
+        public override DateTime ReadJson(
+            JsonReader reader,
+            Type objectType,
+            DateTime existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            return DateTime.ParseExact(
+                (string)reader.Value,
+                Format,
+                System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
